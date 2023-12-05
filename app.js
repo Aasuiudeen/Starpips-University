520ac761bcadf9c7eba27c98533a0f44f07c2e91
Abdullahi Suiudeen <Abdullahi Suiudeen>
2023-12-05 11:39:42 +0100

    carousel

--- app.js
+++ app.js
@@ -23,8 +23,20 @@ buttons.forEach((button) => {
 // Client scroll
 
 const carousel = document.querySelector(".client-card-holder");
-arrowIcon = document.querySelector(".client-title-icon i");
+// arrowIcon = document.querySelector(".client-title-icon i");
 firstCard = document.querySelectorAll(".client-card")[0];
+const leftIcon = document.getElementById("right");
+const rightIcon = document.getElementById("left");
+
+leftIcon.addEventListener("click", () => {
+  carousel.scrollLeft += 900;
+  carousel.style.scrollBehavior = "smooth";
+});
+
+rightIcon.addEventListener("click", () => {
+  carousel.style.scrollBehavior = "smooth";
+  carousel.scrollLeft -= 900;
+});
 
 let isDragstart = false,
   prevPageX,
@@ -41,6 +53,7 @@ const dragStart = (e) => {
   isDragstart = true;
   prevPageX = e.pageX;
   prevScrollLeft = carousel.scrollLeft;
+  carousel.style.scrollBehavior = "smooth";
 };
 const dragging = (e) => {
   if (!isDragstart) {
@@ -49,9 +62,11 @@ const dragging = (e) => {
   e.preventDefault();
   let positionDiff = e.pageX - prevPageX;
   carousel.scrollLeft = prevScrollLeft - positionDiff;
+  carousel.style.scrollBehavior = "smooth";
 };
 const dragStop = () => {
   isDragstart = false;
+  carousel.style.scrollBehavior = "smooth";
 };
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
